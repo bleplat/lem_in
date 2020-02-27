eb5d09ab9728f2a6ef4b23eebe2a3c1e7e19a301
Yzarack <jordan.thiercelin@gmail.com>
2020-02-27 13:52:29 +0100

    include/li_room.h

---
+++ includes/li_room.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   li_room.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jthierce <jthierce@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/26 22:23:36 by jthierce          #+#    #+#             */
+/*   Updated: 2020/02/26 22:25:53 by jthierce         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LI_ROOM_H
+# define LI_ROOM_H
+
+typedef struct	s_rooms
+{
+	char			*name;
+	int				index;
+	int				*link;
+	struct s_rooms	*prev;
+	int				ants_in_path;
+	int				status;
+	struct s_djik	*next;
+}				t_rooms;
+
+#endif
