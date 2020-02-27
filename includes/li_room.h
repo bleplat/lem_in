33042cad675871c3ef7a465e66393a3baf5c8650
Yzarack <jordan.thiercelin@gmail.com>
2020-02-27 16:57:26 +0100

    t_array a la place de int *

--- includes/li_room.h
+++ includes/li_room.h
@@ -6,18 +6,19 @@
 /*   By: jthierce <jthierce@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/26 22:23:36 by jthierce          #+#    #+#             */
-/*   Updated: 2020/02/27 14:30:22 by jthierce         ###   ########.fr       */
+/*   Updated: 2020/02/27 16:55:56 by jthierce         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LI_ROOM_H
 # define LI_ROOM_H
+#include "libft.h"
 
 typedef struct	s_rooms
 {
 	char			*name;
 	int				index;
-	int				*link;
+	t_array			*link;
 	struct s_rooms	*prev;
 	int				ants_in_path; //peut etre inutile a voir plus tard
 	int				status;
