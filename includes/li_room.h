457bd44525b17d07f7bd0229b78cac4cc7d81df0
Yzarack <jordan.thiercelin@gmail.com>
2020-02-27 17:32:13 +0100

    li_room.h finisialisation de la struct

--- includes/li_room.h
+++ includes/li_room.h
@@ -6,7 +6,7 @@
 /*   By: jthierce <jthierce@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/26 22:23:36 by jthierce          #+#    #+#             */
-/*   Updated: 2020/02/27 17:15:45 by bleplat          ###   ########.fr       */
+/*   Updated: 2020/02/27 17:30:46 by jthierce         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,22 +29,13 @@ struct							s_li_room
 	// Ce que je lis dans les fichiers
 	int			index; // Si tu accede aux elements via leur index, alors ceci est deja inutile (si tu utilise rooms[5] alors tu sais deja que index == 5)
 	const char	*name;
+	t_array		*link;//Mettre a NULL
+	t_room		*prev;//mettre a NULL
+	int			status;//Initialiser a 0
 	int			x;
 	int			y;
 	// Ce qui est utile lors de la resolution de l'algo
 	
-}
-
-
-typedef struct	s_rooms
-{
-	char			*name;
-	int				index;
-	t_array			*link;
-	struct s_rooms	*prev;
-	int				ants_in_path; //peut etre inutile a voir plus tard
-	int				status;
-	struct s_rooms	*next;
-}				t_rooms;
+};
 
 #endif
