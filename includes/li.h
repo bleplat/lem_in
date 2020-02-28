397cf8f14872bef5214ec9125311573ea58022e1
Your Name <bleplat@student.42.fr>
2020-02-28 13:08:47 +0100

    added start and end commands a defines

--- includes/li.h
+++ includes/li.h
@@ -6,7 +6,7 @@
 /*   By: bleplat <bleplat@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/27 19:11:09 by bleplat           #+#    #+#             */
-/*   Updated: 2020/02/27 19:59:47 by bleplat          ###   ########.fr       */
+/*   Updated: 2020/02/28 13:08:10 by bleplat          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,9 @@
 # include "li_board.h"
 # include "li_link.h"
 # include "li_room.h"
+
+# define LI_CMD_START "##start\n"
+# define LI_CMD_END "##end\n"
 
 int						li_perror(int code, const char *message);
 
