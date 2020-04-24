eebee56124a2b80befbb751423366609c2fb6b92
Yzarack <jordan.thiercelin@gmail.com>
2020-04-24 18:15:06 +0200

    norme srcs

--- srcs/li_calc_steps.c
+++ srcs/li_calc_steps.c
@@ -6,98 +6,13 @@
 /*   By: jthierce <jthierce@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/28 13:31:01 by jthierce          #+#    #+#             */
-/*   Updated: 2020/04/24 15:01:11 by jthierce         ###   ########.fr       */
+/*   Updated: 2020/04/24 18:14:38 by jthierce         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <limits.h>
 
 #include "li_resolve.h"
-
-static int li_total_step(int *calc, ssize_t size)
-{
-	int		total;
-	ssize_t	i;
-
-	total = 0;
-	i = 0;
-	while (i < size - 1)
-	{
-		total += calc[i];
-		i++;
-	}
-	return (total);
-}
-
-static void li_delete_step(int *calc, int step_destroy, ssize_t size)
-{
-	ssize_t i;
-
-	while (step_destroy > 0)
-	{
-		i = 0;
-		while (i < size)
-			i++;
-		while (--i != -1)
-		{
-			step_destroy--;
-			calc[i]--;
-		}
-	}
-}
-
-static void	li_add_step(int *calc, int step_add, ssize_t size)
-{
-	ssize_t i;
-
-	while (step_add > 0)
-	{
-		i = 0;
-		while (step_add != 0 && i < size)
-		{
-			if (calc[i] == -2)
-				calc[i] = 1;
-			else
-				calc[i]++;
-			i++;
-			step_add--;
-		}
-	}
-}
-
-int li_calc(t_calc_step step, t_board board)
-{
-	int		*calc;
-	ssize_t	i;
-	int		j;
-
-	i = -1;
-	if (!(calc = (int *)malloc(sizeof(int) * (step.size))))
-		return (-1);
-	while (++i < step.size)
-		calc[i] = 0;
-	i = -1;
-	while (++i < step.size)
-	{
-		if (i + 1 < step.size)
-		{
-			j = i + 1;
-			while (--j != -1)
-				calc[j] += step.distance[i + 1] - step.distance[i];
-		}
-		else
-			calc[i] = -2;
-	}
-	j = li_total_step(calc, step.size);
-	j = board.ants_count - j;
-	if (j < 0)
-		li_delete_step(calc, j * -1, step.size);
-	else if (j > 0)
-		li_add_step(calc, j, step.size);
-	j = step.distance[0] + calc[0] - 1;
-	free(calc);
-	return (j);
-}
 
 /*
 ** Calcul le nombre d'etape minimum avec les nouveaux chemins.
